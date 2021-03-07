bbf2c230739f075f69ad475812ad9101ed25d1bd
Fernando <orellanadani12@gmail.com>
2021-03-07 00:25:15 +0000

    Work on getting the servlet to run

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 
-@WebServlet("/hello")
+@WebServlet("/hi")
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
